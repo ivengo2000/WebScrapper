c907d8d3abfb93ea1c4392ee9a5bd42ea756992b
Sergey Ivanov <sergey.ivanov@perficient.com>
2025-06-02 17:51:23 +0300

    remove from incorrect folder

--- Models/MediaPageListResultModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace WebScrapper.Models
-{
-    public class MediaPageListResultModel: MediaPageListResultModelBase
-    {
-        public List<MediaPageListModel> Pages { get; set; }
-    }
-}
